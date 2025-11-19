39b504498f048c0281cf0469efca99adf567a063
Samuel Rodríguez <samuelrodriguezrey@outlook.com>
2025-11-19 17:55:19 +0100

    feat: upload cypress videos as artifacts

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50 mb-6">
         Proyecto final con Github Actions
       </h1>
-      <img src="" alt="" />
       <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400 mb-6">
         Resultado:{" "}
         <span
